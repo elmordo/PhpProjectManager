c4ea5ac94bbd709e05abc4820e2af766eebc12af
Ing. Petr Jindra <el.mordo@gmail.com>
2017-05-23 08:25:57 +0200

    positional arguments are always required

--- src/PPM/Router/Parser/Positional.php
+++ src/PPM/Router/Parser/Positional.php
@@ -7,6 +7,19 @@ class Positional extends AArgument
 {
 
     /**
+     * set requried flag to true
+     * @param string|null $name argument name
+     * @param string|null $description description
+     * @param string|null $defaultValue default value
+     * @return type
+     */
+    public function __construct(string $name=null, string $description=null, string $defaultValue=null)
+    {
+        parent::__construct($name, $description, $defaultValue);
+        $this->required = true;
+    }
+
+    /**
      * any available item is valid
      * @param Data $data data to parse
      * @return string parsed value
@@ -23,5 +36,13 @@ class Positional extends AArgument
 
 		return $this;
 	}
+
+    public function setRequired(bool $value) : AArgument
+    {
+        if (!$value)
+            throw new \InvalidArgumentException("Positional arguments are always required");
+
+        return parent::setRequired($value);
+    }
 
 }
