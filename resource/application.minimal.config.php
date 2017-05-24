b8c0ebf396dc58fd095e5fb37ed569e88026c9bf
Ing. Petr Jindra <el.mordo@gmail.com>
2017-05-24 08:08:14 +0200

    minimal config update (new routes)

--- resource/application.minimal.config.php
+++ resource/application.minimal.config.php
@@ -2,7 +2,7 @@
 
 return [
     "routes" => [
-        // app initialization
+        // route app initialization
         [
             "name" => "init",
             "defaults" => [
@@ -18,5 +18,34 @@ return [
                 ], // end argument init
             ], // end definition
         ], // end route init
+
+        // route help (by empty)
+        [
+            "name" => "help_empty",
+            "defaults" => [
+                "controller" => "help",
+                "action" => "index"
+            ],
+            "definition" => [
+            ], // end definition
+        ], // end help empty
+
+        // route help (by help command)
+        [
+            "name" => "help",
+            "defaults" => [
+                "controller" => "help",
+                "action" => "index"
+            ],
+            "definition" => [
+                [
+                    "type" => "static",
+                    "options" => [
+                        "name" => "help"
+                    ]
+                ]
+            ], // end definition
+        ], // end help empty
+
     ],
 ];
