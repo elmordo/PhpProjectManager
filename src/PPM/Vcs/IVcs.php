d41580c8dab104e73a6a98b1c64bd0fbe59ef5d5
Ing. Petr Jindra <el.mordo@gmail.com>
2017-05-29 10:05:29 +0200

    VCS interface extended

--- src/PPM/Vcs/IVcs.php
+++ src/PPM/Vcs/IVcs.php
@@ -7,6 +7,43 @@ interface IVcs
 {
 
     /**
+     * get current base path
+     * @return string base path
+     */
+    public function getBasePath() : string;
+
+    /**
+     * set new base path
+     * @param string $basePath new base path
+     * @return IVcs reference to this instance
+     */
+    public function setBasePath(string $basePath) : IVcs;
+
+    /**
+     * test if repository is initialized in base path
+     * @return boolean true if initialized, false otherwise
+     */
+    public function isInitialized() : bool;
+
+    /**
+     * initialize repository in base path
+     * @return IVcs reference to this instance
+     */
+    public function initialize() : IVcs;
+
+    /**
+     * get list of changed files
+     * @return array list of changed files
+     */
+    public function getChangeds() : array;
+
+    /**
+     * add changes into next commit
+     * @param string $pattern pattern to filter items
+     */
+    public function add(string $pattern) : IVcs;
+
+    /**
      * push data to server
      * @return IVcs reference to this instance
      */
