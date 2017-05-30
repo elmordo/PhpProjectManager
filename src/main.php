ac50ce44c44c0f93a7ff48f2e0a3fe66995ee5b0
Ing. Petr Jindra <el.mordo@gmail.com>
2017-05-30 09:52:45 +0200

    fix of missing config

--- src/main.php
+++ src/main.php
@@ -27,6 +27,7 @@ $globalConfigPath = joinPath($currentPath, \PPM\Controller\ProjectController::AP
 // setup application config
 $configs = [
     joinPath(__DIR__, "../resource/application.minimal.config.php"),
+    joinPath(__DIR__, "../resource/application.default.config.php"),
     $globalConfigPath,
     joinPath($currentPath, \PPM\Controller\ProjectController::APP_LOCAL_CONFIG),
 ];
