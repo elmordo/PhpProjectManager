129ff570e0d562856b99804294b2e07847790925
Ing. Petr Jindra <el.mordo@gmail.com>
2017-05-30 08:51:14 +0200

    update of Application

--- src/PPM/Controller/ApplicationController.php
+++ src/PPM/Controller/ApplicationController.php
@@ -3,23 +3,33 @@
 namespace PPM\Controller;
 
 
-class ApplicationController extends ApplicationController
+class ApplicationController extends AController
 {
 
-    const UPLOAD_PATH = "https://github.com/elmordo/PhpProjectManager.git";
+    //const REPOSITORY_URL = "https://github.com/elmordo/PhpProjectManager.git";
+    const REPOSITORY_URL = "/home/petr/Projekty/PhpProjectManager";
+
+    const CMD_CLONE = "git clone {url} {dir}";
+
+    const CMD_CHECKOUT = "cd {dir} && git checkout {version}";
 
     public function updateAction()
     {
-        $path = $this->cloneRepo();
+        $repoPath = $this->cloneRepo();
+        $release = $this->checkoutRelease($repoPath);
     }
 
-    private function cloneRepo()
+    private function cloneRepo() : string
     {
         $tmpDir = $this->createTempDir();
 
+        $cmd = $this->prepareCloneCommand($tmpDir);
+        passthru($cmd);
+
+        return $tmpDir;
     }
 
-    public function createTempDir()
+    private function createTempDir() : string
     {
         $tmpDir = sys_get_temp_dir();
         $tmpName = tempnam($tmpDir, "ppm");
@@ -30,6 +40,55 @@ class ApplicationController extends ApplicationController
         mkdir($tmpName);
 
         return $tmpName;
+    }
+
+    private function prepareCloneCommand(string $targetDir) : string
+    {
+        $params = [ "{url}" => self::REPOSITORY_URL, "{dir}" => $targetDir ];
+        $cmd = strtr(self::CMD_CLONE, $params);
+        return $cmd;
+    }
+
+    /**
+     * checkout last release and return version
+     * @param string $repoPath [description]
+     * @return [type] [description]
+     */
+    private function checkoutRelease(string $repoPath) : string
+    {
+        $release = $this->getVersion($repoPath);
+        $command = $this->prepareCheckoutCommand($repoPath, $release);
+
+        passthru($command);
+
+        return $release;
+    }
+
+    private function prepareCheckoutCommand(string $repoPath, string $version) : string
+    {
+        $params = [ "{dir}" => $repoPath, "{version}" => $version ];
+        $cmd = strtr(self::CMD_CHECKOUT, $params);
+
+        return $cmd;
+    }
+
+    /**
+     * load version from VERSION file
+     * @param string $path path where VERSION is located
+     * @return string version number
+     */
+    public function getVersion(string $path) : string
+    {
+        try
+        {
+            $version = file_get_contents(joinPath($path, "VERSION"));
+        }
+        catch (\Exception $e)
+        {
+            throw new \RuntimeException("Unable to determine version number of downloaded repository");
+        }
+
+        return $version;
     }
 
 }
